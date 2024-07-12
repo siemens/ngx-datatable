8b747a8c2082a3bd6bc9091df153b738119ab924
lecorsim <145570314+lecorsim@users.noreply.github.com>
2024-07-12 17:39:30 +0200

    Update sticky-header.directive.ts
    Refacto

--- projects/ngx-datatable/src/lib/directives/sticky-header.directive.ts
+++ projects/ngx-datatable/src/lib/directives/sticky-header.directive.ts
@@ -16,33 +16,35 @@ export class StickyHeaderDirective implements OnInit {
     }
   }
 
-  @HostListener('window:scroll', ['$event'])
+    @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
-    const datatableHeader = this.ngxDatatable?.element?.querySelector('.datatable-header') as HTMLElement;
-    const datatableHeaderBoundingClientRect = datatableHeader.getBoundingClientRect();
-    const datatableBoundingClientRect = this.ngxDatatable?.element?.getBoundingClientRect();
-    const datatableYTopInViewport = this._isFixed
-      ? datatableBoundingClientRect.top - datatableHeaderBoundingClientRect.height
-      : datatableHeaderBoundingClientRect.top;
+    const header = this.ngxDatatable?.element?.querySelector('.datatable-header') as HTMLElement;
+    const headerRect = header.getBoundingClientRect();
+    const tableRect = this.ngxDatatable?.element?.getBoundingClientRect();
 
-    // 2 * datatableHeaderBoundingClientRect?.height is used to account for the height of the potential sticky header
-    // and the height of the header that is now unfixed (so the table was "pushed" by the top by the header that is now unfixed)
-    const datatableYBottomInViewport = datatableBoundingClientRect.bottom - datatableHeaderBoundingClientRect?.height;
-
-    if (datatableYTopInViewport <= 0 && datatableYBottomInViewport > 0) {
-      datatableHeader.style.position = 'fixed';
-      datatableHeader.style.top = '0';
-      datatableHeader.style.zIndex = '1000';
-      datatableHeader.style.backgroundColor = 'white';
-      this.ngxDatatable.element.style.marginTop = `${datatableHeaderBoundingClientRect?.height}px`;
-      this._isFixed = true;
-    } else {
-      datatableHeader.style.position = null;
-      datatableHeader.style.top = null;
-      datatableHeader.style.zIndex = null;
-      datatableHeader.style.backgroundColor = null;
-      this.ngxDatatable.element.style.marginTop = null;
-      this._isFixed = false;
+    const shouldFixHeader = tableRect.top <= 0 && tableRect.bottom - headerRect.height > 0;
+    if (shouldFixHeader && !this.isFixed) {
+      this.applyFixedStyles(header);
+      this.isFixed = true;
+    } else if (!shouldFixHeader && this.isFixed) {
+      this.resetStyles(header);
+      this.isFixed = false;
     }
+  }
+
+  private applyFixedStyles(header: HTMLElement) {
+    header.style.position = 'fixed';
+    header.style.top = '0';
+    header.style.zIndex = '1000';
+    header.style.backgroundColor = 'white';
+    this.ngxDatatable.element.style.marginTop = `${header.getBoundingClientRect().height}px`;
+  }
+
+  private resetStyles(header: HTMLElement) {
+    header.style.position = null;
+    header.style.top = null;
+    header.style.zIndex = null;
+    header.style.backgroundColor = null;
+    this.ngxDatatable.element.style.marginTop = null;
   }
 }

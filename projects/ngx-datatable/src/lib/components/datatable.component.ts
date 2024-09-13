16306cd82dab149b2684600abe836038fe00ef30
Chintan Kavathia <chintan.kavathia@gmail.com>
2024-09-13 15:16:16 +0530

    refactor: remove unnecessary getter/setters on columnTemplates

--- projects/ngx-datatable/src/lib/components/datatable.component.ts
+++ projects/ngx-datatable/src/lib/components/datatable.component.ts
@@ -628,17 +628,7 @@ export class DatatableComponent<TRow = any> implements OnInit, DoCheck, AfterVie
    * if described in your markup.
    */
   @ContentChildren(DataTableColumnDirective)
-  set columnTemplates(val: QueryList<DataTableColumnDirective>) {
-    this._columnTemplates = val;
-    this.translateColumns(val);
-  }
-
-  /**
-   * Returns the column templates.
-   */
-  get columnTemplates(): QueryList<DataTableColumnDirective> {
-    return this._columnTemplates;
-  }
+    columnTemplates!: QueryList<DataTableColumnDirective>;
 
   /**
    * Row Detail templates gathered from the ContentChild
@@ -711,7 +701,6 @@ export class DatatableComponent<TRow = any> implements OnInit, DoCheck, AfterVie
   _internalRows: TRow[];
   _internalColumns: TableColumn[];
   _columns: TableColumn[];
-  _columnTemplates: QueryList<DataTableColumnDirective>;
   _subscriptions: Subscription[] = [];
   _ghostLoadingIndicator = false;
   protected verticalScrollVisible = false;
@@ -789,7 +778,10 @@ export class DatatableComponent<TRow = any> implements OnInit, DoCheck, AfterVie
    * content has been fully initialized.
    */
   ngAfterContentInit() {
-    this.columnTemplates.changes.subscribe(v => this.translateColumns(v));
+    if (this.columnTemplates.length) {
+      this.translateColumns(this.columnTemplates);
+    }
+    this._subscriptions.push(this.columnTemplates.changes.subscribe(v => this.translateColumns(v)));
     this.listenForColumnInputChanges();
   }
 

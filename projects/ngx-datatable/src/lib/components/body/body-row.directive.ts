71325c856e066786eb4579dec3e9042819e1ecc9
Chintan Kavathia <chintankumar.kavathia@siemens.com>
2025-02-28 16:47:25 +0530

    refactor: remove duplicate template code

---
+++ projects/ngx-datatable/src/lib/components/body/body-row.directive.ts
@@ -0,0 +1,19 @@
+import { Directive } from '@angular/core';
+import { Group, TreeStatus } from '../../types/public.types';
+
+@Directive({
+  selector: '[ngx-datatable-body-row]',
+  standalone: true
+})
+export class DatatableBodyRowDirective {
+  static ngTemplateContextGuard<TRow extends { treeStatus?: TreeStatus } = any>(
+    directive: DatatableBodyRowDirective,
+    context: unknown
+  ): context is {
+    row: TRow;
+    groupValue: Group<TRow>[];
+    index: number;
+  } {
+    return true;
+  }
+}

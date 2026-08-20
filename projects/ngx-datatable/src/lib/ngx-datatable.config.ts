8f82f90d975465501188afe178e9b5806ab8735c
Maximilian Koeller <maximilian.koeller@siemens.com>
2026-08-20 09:30:58 +0200

    fix: restore partial typing for all NgxDatatableConfig fields

--- projects/ngx-datatable/src/lib/ngx-datatable.config.ts
+++ projects/ngx-datatable/src/lib/ngx-datatable.config.ts
@@ -3,72 +3,59 @@ import { InjectionToken, Provider } from '@angular/core';
 /** Interface for messages to override default table texts. */
 export interface NgxDatatableMessages {
   /** Message to show when the array is present but empty */
-  emptyMessage: string;
+  emptyMessage?: string;
   /** Footer total message */
-  totalMessage: string;
+  totalMessage?: string;
   /** Footer selected message */
-  selectedMessage: string;
+  selectedMessage?: string;
   /** Pager screen reader message for the first page button */
-  ariaFirstPageMessage: string;
+  ariaFirstPageMessage?: string;
   /**
    * Pager screen reader message for the n-th page button.
    * It will be rendered as: `{{ariaPageNMessage}} {{n}}`.
    */
-  ariaPageNMessage: string;
+  ariaPageNMessage?: string;
   /** Pager screen reader message for the previous page button */
-  ariaPreviousPageMessage: string;
+  ariaPreviousPageMessage?: string;
   /** Pager screen reader message for the next page button */
-  ariaNextPageMessage: string;
+  ariaNextPageMessage?: string;
   /** Pager screen reader message for the last page button */
-  ariaLastPageMessage: string;
+  ariaLastPageMessage?: string;
   /** Row checkbox aria label */
-  ariaRowCheckboxMessage: string;
+  ariaRowCheckboxMessage?: string;
   /** Header checkbox aria label */
-  ariaHeaderCheckboxMessage: string;
+  ariaHeaderCheckboxMessage?: string;
   /** Group header checkbox aria label */
-  ariaGroupHeaderCheckboxMessage: string;
+  ariaGroupHeaderCheckboxMessage?: string;
   /** Loading indicator aria label */
-  ariaLoadingMessage: string;
+  ariaLoadingMessage?: string;
 }
 
 /** CSS classes for icons that override the default table icons. */
 export interface NgxDatatableCssClasses {
-  sortAscending: string;
-  sortDescending: string;
-  sortUnset: string;
-  pagerLeftArrow: string;
-  pagerRightArrow: string;
-  pagerPrevious: string;
-  pagerNext: string;
-  treeStatusLoading: string;
-  treeStatusExpanded: string;
-  treeStatusCollapsed: string;
+  sortAscending?: string;
+  sortDescending?: string;
+  sortUnset?: string;
+  pagerLeftArrow?: string;
+  pagerRightArrow?: string;
+  pagerPrevious?: string;
+  pagerNext?: string;
+  treeStatusLoading?: string;
+  treeStatusExpanded?: string;
+  treeStatusCollapsed?: string;
 }
 
-/**
- * Interface definition for ngx-datatable global configuration
- */
+/** Interface definition for ngx-datatable global configuration. */
 export interface NgxDatatableConfig {
-  messages: NgxDatatableMessages;
-  cssClasses: NgxDatatableCssClasses;
-  headerHeight: number | 'auto';
-  footerHeight: number;
-  rowHeight: number | 'auto' | ((row: any) => number);
-  defaultColumnWidth: number;
+  messages?: NgxDatatableMessages;
+  cssClasses?: NgxDatatableCssClasses;
+  headerHeight?: number | 'auto';
+  footerHeight?: number;
+  rowHeight?: number | 'auto' | ((row: any) => number);
+  defaultColumnWidth?: number;
 }
 
-export const NGX_DATATABLE_CONFIG = new InjectionToken<AllPartial<NgxDatatableConfig>>(
-  'ngx-datatable.config'
-);
-
-/**
- * This makes all properties recursively optional.
- *
- * @internal
- */
-export type AllPartial<T> = T extends (...args: any[]) => any
-  ? T
-  : { [K in keyof T]?: AllPartial<T[K]> };
+export const NGX_DATATABLE_CONFIG = new InjectionToken<NgxDatatableConfig>('ngx-datatable.config');
 
 /**
  * Interface definition for INgxDatatableConfig global configuration.
@@ -82,7 +69,7 @@ export type INgxDatatableConfig = NgxDatatableConfig;
  *
  * @param overrides The overrides of the table configuration.
  */
-export const providedNgxDatatableConfig = (overrides: AllPartial<NgxDatatableConfig>): Provider => {
+export const providedNgxDatatableConfig = (overrides: NgxDatatableConfig): Provider => {
   return {
     provide: NGX_DATATABLE_CONFIG,
     useValue: overrides
